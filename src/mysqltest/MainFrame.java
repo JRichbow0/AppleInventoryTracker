afe551763883cb6207f1dc8ef196c9a0efbfe325
jrichb2 <jrichb2@cssail15.towson.edu>
2022-05-09 13:44:26 -0400

    Adding source code & jar file

--- src/mysqltest/MainFrame.java
+++ src/mysqltest/MainFrame.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package mysqltest;
-import OtherGUI.DeviceFrame;
 import com.mysql.cj.jdbc.result.ResultSetMetaData;
 import java.sql.Connection;
 import java.sql.DriverManager;
